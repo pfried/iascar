81bd024aa4f35a1cfb353c3aff9a41c17ab55b41
Friedrich Maeckle <friedrich.maeckle@gmail.com>
2014-07-01 08:53:53 +0200

    enhancement - adding ngTouch

--- gruntfile.js
+++ gruntfile.js
@@ -6,9 +6,10 @@ module.exports = function (grunt) {
     var cordovaBuildPath = 'bluetoothcar/www';
 
     var CLIENT_LIB_FILES = [
-        'src/js/lib/bower/jquery/dist/jquery.js',
+        //'src/js/lib/bower/jquery/dist/jquery.js',
         'src/js/lib/bower/angular/angular.js',
         'src/js/lib/bower/angular-cookies/angular-cookies.js',
+        'src/js/lib/bower/angular-touch/angular-touch.js',
         'src/js/lib/bower/angular-resource/angular-resource.js',
         'src/js/lib/bower/angular-bootstrap/ui-bootstrap.js',
         'src/js/lib/bower/angular-ui-router/release/angular-ui-router.js',
